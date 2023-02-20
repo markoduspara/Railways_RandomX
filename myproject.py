c60b5f75ca4ca004b44301a2a06a1e14fbc6fdee
markoduspara <37013131+markoduspara@users.noreply.github.com>
2023-02-20 11:27:56 +0100

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,4 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
